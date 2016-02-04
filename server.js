a183a61bf6071db7ad628702978f8de1a237f98a
Rory <rory88@gmail.com>
2016-02-04 09:13:02 +0000

    fixed port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
 	res.send('got \'/\'')
 });
 
-var port = Number(process.env.NODE_ENV || 3000)
+var port = Number(process.env.PORT || 3000)
 
 app.listen(port, () => {
 	console.log('listening on ' + port)
