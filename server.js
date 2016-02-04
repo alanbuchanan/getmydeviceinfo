67a73b916cd5690e7563e5d98f2e55e4c125ec03
Rory <rory88@gmail.com>
2016-02-04 09:53:37 +0000

    tried hack to fix lang

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
 	console.log('headers:', req.headers)
 
 	var fullLang = req.headers['accept-language'];
-	var fullSoftware = req.headers['user-agent'];
+	var fullSoftware = req.headers['user-agent'] += ',';
 
 	var ip = req.headers['x-forwarded-for'],
 		lang = fullLang.substr(0, fullLang.indexOf(',')),
